dac1009aec8984b4ae7d03cbb9c4bee0200aa6cd
alexhoefner <xwerbung@yahoo.de>
2019-03-26 15:23:59 +0100

    9/10 variables

--- workload/3/main.js
+++ workload/3/main.js
@@ -1,3 +1,4 @@
-let myName = "Alex";
-let myCity = "Holzkirchen";
-console.log(`My name is ${myName}. My favorite city is ${myCity}.`)
+let newVariable = 'Playing around with typeof.';
+console.log(typeof newVariable);
+newVariable = 1;
+console.log(typeof newVariable);
