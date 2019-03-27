a072a5b57b4b0c0ce1815e50676e629ec08b2d54
Höfner <csav9726@uibk.ac.at>
2019-03-27 18:16:04 +0100

    konst adlerblicke

--- adlerweg/main.js
+++ adlerweg/main.js
@@ -28,63 +28,47 @@ L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png").addTo(karte);
 
 //Positionsmarker hinzufügen
 let pin1 = L.marker(
-    [breite1,laenge1]
+    [breite1, laenge1]
 ).addTo(karte);
 let pin2 = L.marker(
-    [breite2,laenge2]
+    [breite2, laenge2]
 ).addTo(karte);
 
 // Popup zum Pin hängen
-pin1.bindPopup(titel1).openPopup();
+// pin1.bindPopup(titel1).openPopup();
 
-const blick1 = { 
-    kunde: "Wilder Kaiser ", 
-    standort: "Gruttenhütte",
-    seehoehe: 1640,
-    lat: 47.55564,
-    lng: 12.31861
-};
+// Adlerblicke als Const definieren
+const adlerblicke = [{
+        kunde: "Wilder Kaiser ",
+        standort: "Gruttenhütte",
+        seehoehe: 1640,
+        lat: 47.55564,
+        lng: 12.31861
+    },
+    {
+        kunde: "Bergbahn Scheffau",
+        standort: "Brandstadl",
+        seehoehe: 1640,
+        lat: 47.4912,
+        lng: 12.248
+    },
+    {
+        kunde: "Lechtal Tourismus",
+        standort: "Sonnalm Jöchelspitze",
+        seehoehe: 1786,
+        lat: 47.27528,
+        lng: 10.36505
+    }
+]
 
-const blick2 = { 
-    kunde: "Bergbahn Scheffau", 
-    standort: "Brandstadl",
-    seehoehe: 1640,
-    lat: 47.4912,
-    lng: 12.248
-};
+// for-Schleife für adlerblicke
+for (let blick of adlerblicke) {
+    let blickpin = L.marker(
+        [blick.lat, blick.lng]
+    ).addTo(karte);
 
-const blick3 = { 
-    kunde: "Lechtal Tourismus", 
-    standort: "Sonnalm Jöchelspitze",
-    seehoehe: 1786,
-    lat: 47.27528,
-    lng: 10.36505
-
-};
-
-let pin3 = L.marker(
-    [blick1.lat,blick1.lng]
-).addTo(karte);
-
-let pin4 = L.marker(
-    [blick2.lat,blick2.lng]
-).addTo(karte);
-
-let pin5 = L.marker(
-    [blick3.lat,blick3.lng]
-).addTo(karte);
-
-pin3.bindPopup(`<h1>Standort ${blick1.standort}</h1>
-    <p>Höhe ${blick1.seehoehe}</p>
-    <em>Kunde ${blick1.kunde}</em>`
-    );
-
-pin4.bindPopup(`<h1>Standort ${blick2.standort}</h1>
-    <p>Höhe ${blick2.seehoehe}</p>
-    <em>Kunde ${blick2.kunde}</em>`
-    );
-    
-pin5.bindPopup(`<h1>Standort ${blick3.standort}</h1>
-    <p>Höhe ${blick3.seehoehe}</p>
-    <em>Kunde ${blick3.kunde}</em>`
-    );    
+    blickpin.bindPopup(
+        `<h1>Standort: ${blick.standort}</h1>
+    <p>Höhe: ${blick.seehoehe}</p>
+    <em>Kunde: ${blick.kunde}</em>`);
+}
