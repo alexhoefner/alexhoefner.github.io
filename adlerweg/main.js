b295368d35ee3c665e4a7451cb8ca30199fadc13
Höfner <csav9726@uibk.ac.at>
2019-03-27 17:02:58 +0100

    adlerweg

--- adlerweg/main.js
+++ adlerweg/main.js
@@ -2,9 +2,13 @@
 
 const div = document.getElementById("map")
 
-const breite = div.getAttribute("data-lat");
-const laenge = div.getAttribute("data-lng");
-const titel = div.getAttribute("data-title");
+const breite1 = div.getAttribute("data-lat1");
+const laenge1 = div.getAttribute("data-lng1");
+const titel1 = div.getAttribute("data-title1");
+const breite2 = div.getAttribute("data-lat2");
+const laenge2 = div.getAttribute("data-lng2");
+const titel2 = div.getAttribute("data-title2");
+
 
 // console.log("Breite=",breite,"Länge=",laenge,"Titel=",titel);
 
@@ -15,17 +19,20 @@ let karte = L.map("map");
 
 // auf Ausschnitt zoomen
 karte.setView(
-    [breite, laenge],
-    13
+    [breite1, laenge1],
+    12
 );
 
 // OSM
 L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png").addTo(karte);
 
 //Positionsmarker hinzufügen
-let pin = L.marker(
-    [breite,laenge]
+let pin1 = L.marker(
+    [breite1,laenge1]
+).addTo(karte);
+let pin2 = L.marker(
+    [breite2,laenge2]
 ).addTo(karte);
 
 // Popup zum Pin hängen
-pin.bindPopup(titel).openPopup();
+pin1.bindPopup(titel1).openPopup();
