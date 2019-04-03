8fcc2a976b3fca8ec4dcfd291aeffbff24f7a6ce
Höfner <csav9726@uibk.ac.at>
2019-04-03 16:20:20 +0200

    footer

--- adler/main.js
+++ adler/main.js
@@ -24,9 +24,16 @@ karte.setView(
 );
 
 // OSM
-L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png", {
-    subdomains: ['a','b','c'],
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'
+// L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png", {
+//     subdomains: ['a','b','c'],
+//     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'
+// }).addTo(karte);
+
+
+
+L.tileLayer("https://{s}.wien.gv.at/basemap/geolandbasemap/normal/google3857/{z}/{y}/{x}.png", {
+    subdomains: ['maps','maps1','maps2','maps3','maps4'],
+    attribution: 'Datenquelle: <a href="https://www.basemap.at">basemap.at</a>'
 }).addTo(karte);
 
 //Positionsmarker hinzufügen
