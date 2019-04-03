ed1c7558e444511d5a67ef83b521dd1d8d389d30
Höfner <csav9726@uibk.ac.at>
2019-04-03 18:13:55 +0200

    leaflet hash einbinden

--- adler/main.js
+++ adler/main.js
@@ -135,3 +135,13 @@ karte.addControl(new L.Control.Fullscreen());
 
 // Leaflet hash Plugin einbinden (Koordinaten in URL)
 var hash = new L.Hash(karte);
+
+
+// Coordinates Control
+var c = new L.Control.Coordinates(); // you can send options to the constructor if you want to, otherwise default values are used
+
+c.addTo(karte);
+
+map.on('click', function(e) {
+	c.setCoordinates(e);
+});
