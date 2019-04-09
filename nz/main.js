cd1f5d5800631fbf2dbd328d22631590fad71045
Höfner <csav9726@uibk.ac.at>
2019-04-09 10:25:29 +0200

    Add Kartenlayer

--- nz/main.js
+++ nz/main.js
@@ -19,8 +19,46 @@ karte.setView(
     13
 );
 
-// OSM
-L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png").addTo(karte);
+// OSM und weitere Layer hinzufügen
+// L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png").addTo(karte);
+
+const kartenLayer = {
+    osm: L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png", {
+        subdomains: ['a','b','c'],
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'
+    }),
+    stamen_toner: L.tileLayer("https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}.png", {
+        subdomains: ['a','b','c'],
+        attribution: 'Map tiles by <a href="https://stamen.com/">Stamen Design</a>, under <a href="https://creativecommons.org/licenses/by/3.0/">CC BY 3.0</a>. Data by <a href="https://www.openstreetmap.org/#map=8/47.714/13.349">OpenStreetMap</a> under <a href="https://www.openstreetmap.org/copyright"ODbL</a>'
+    }),
+    stamen_terrain: L.tileLayer("https://stamen-tiles-{s}.a.ssl.fastly.net/terrain/{z}/{x}/{y}.jpg", {
+        subdomains: ['a','b','c'],
+        attribution: 'Map tiles by <a href="https://stamen.com/">Stamen Design</a>, under <a href="https://creativecommons.org/licenses/by/3.0/">CC BY 3.0</a>. Data by <a href="https://www.openstreetmap.org/#map=8/47.714/13.349">OpenStreetMap</a> under <a href="https://www.openstreetmap.org/copyright"ODbL</a>'
+    }),
+    stamen_watercolor: L.tileLayer("https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.jpg", {
+        subdomains: ['a','b','c'],
+        attribution: 'Map tiles by <a href="https://stamen.com/">Stamen Design</a>, under <a href="https://creativecommons.org/licenses/by/3.0/">CC BY 3.0</a>. Data by <a href="https://www.openstreetmap.org/#map=8/47.714/13.349">OpenStreetMap</a> under <a href="https://creativecommons.org/licenses/by-sa/3.0/"CC BY SA</a>'
+    }),
+};
+
+// OSM Basemap einbinden
+// L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png", {
+//     subdomains: ['a','b','c'],
+//     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'
+// }).addTo(karte);
+
+kartenLayer.osm.addTo(karte);
+
+//Kartenmenü hinzufügen
+L.control.layers({
+"Open Street Map": kartenLayer.osm,
+"Stamen Toner": kartenLayer.stamen_toner,
+"Stamen Terrain": kartenLayer.stamen_terrain,
+"Stamen Watercolor": kartenLayer.stamen_watercolor
+
+
+}).addTo(karte);
+
 
 //Positionsmarker hinzufügen
 let pin = L.marker(
