4eec1ba0f4c0734b12a4969ae12e2f55bd466b4a
Höfner <csav9726@uibk.ac.at>
2019-05-08 17:58:25 +0200

    Add Icons Windspeed

--- aws-tirol/main.js
+++ aws-tirol/main.js
@@ -56,7 +56,7 @@ const kartenLayer = {
 
 kartenLayer.osm.addTo(karte);
 
-L.control.layers({
+const layerControl = L.control.layers({
     "Open Street Map": kartenLayer.osm,
     "Geoland Basemap": kartenLayer.geolandbasemap,
     "Geoland Basemap Overlay": kartenLayer.geolandbasemapoverlay,
@@ -79,15 +79,52 @@ karte.setView(
 );
 
 // console.log(AWS);
-const awsTirol = L.featureGroup();
-L.geoJson(AWS)
-    .bindPopup(function(layer){
-        console.log("Layer ",layer);
-        return `Temperatur: ${layer.feature.properties.LT} °C<br>
+
+// Daten vom Server holen
+async function loadStations() {
+    // await: warte auf Aufruf, Daten von Homepage lesen
+    const response = await fetch("https://aws.openweb.cc/stations");
+    // Umwandeln in GeoJson
+    const stations = await response.json();
+    const awsTirol = L.featureGroup();
+    L.geoJson(stations)
+        .bindPopup(function (layer) {
+            // console.log("Layer ",layer);
+            // Datum neu formatieren
+            const date = new Date(layer.feature.properties.date)
+            console.log("Datum", date)
+            return `<h4>${layer.feature.properties.name}</h4>
+        Stationshöhe (m): ${layer.feature.geometry.coordinates[2]} <br>
+        Temperatur: ${layer.feature.properties.LT} °C<br>
         Schneehöhe: ${layer.feature.properties.HS} cm<br>
-        Datum: ${layer.feature.properties.date}`;
-    })
-    .addTo(awsTirol);
-awsTirol.addTo(karte);
-// Ausschnitt
-karte.fitBounds(awsTirol.getBounds());
+        
+        Datum: ${date.toLocaleDateString("de-AT")}
+        | Uhrzeit: ${date.toLocaleTimeString("de-AT")}<br>
+        Windgeschwindigkeit (km/h):
+        
+        ${layer.feature.properties.WG ? layer.feature.properties.WG : "keine Daten"}
+        <hr>
+        <footer> Land Tirol - <a href="https://data.tirol.gv.at">data.tirol.gv.at</a></footer>`;
+            // ? if Abfrage: ?if :else
+        })
+        .addTo(awsTirol);
+    awsTirol.addTo(karte);
+    // Ausschnitt
+    karte.fitBounds(awsTirol.getBounds());
+    layerControl.addOverlay(awsTirol, "Wetterstationen Tirol");
+    L.geoJson(stations, {
+        pointToLayer: function (feature, latlng) {
+            if (feature.properties.WR) {
+                return L.marker(latlng, {
+                    icon: L.divIcon({
+                        html: `<i style="transform: rotate(${feature.properties.WR}deg)" class="fas fa-arrow-circle-up fa-3x"></i>`
+                    
+                    })
+
+                });
+
+            }
+        }
+    }).addTo(karte);
+}
+loadStations();
